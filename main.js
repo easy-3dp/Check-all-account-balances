38100733fac2f7bed5f3d7f66014a65df064b8a1
easy-3dp <113426476+easy-3dp@users.noreply.github.com>
2023-06-29 15:53:05 +0800

    Update main.js

--- main.js
+++ main.js
@@ -19,37 +19,49 @@ async function main(){
     const users = await api.query.system.account.entries();
     for (const user of users) {
         let account = Buffer.from(user[0].slice(-32)).toString('hex');
-        let balance = (user[1].data.free/1000000000000).toString();
-        let transferable = Math.max(0, user[1].data.free-user[1].data.reserved-Math.max(user[1].data.miscFrozen, user[1].data.feeFrozen));
-        let transferableStr = (transferable/1000000000000).toString();
-        console.log(`0x${account}, ${balance}, ${transferableStr}`);
+        let { free, reserved, miscFrozen, feeFrozen } = user[1].data;
+        free = BigInt(free);
+        reserved   = BigInt(reserved);
+        miscFrozen = BigInt(miscFrozen);
+        feeFrozen  = BigInt(feeFrozen);
 
-        total += BigInt(user[1].data.free);
+        let balance = free+reserved;
+        let transferable = free-(miscFrozen > feeFrozen ? miscFrozen : feeFrozen);
+        transferable = 0n > transferable ? 0n : transferable;
+        console.log(`0x${account}, ${balance.toString()/1000000000000}, ${transferable.toString()/1000000000000}`);
 
-        if (account == '608a961b5e9453cf703b18485cfcfb1ad9aed9930da3f80656f27a6ac12b135e'
-         || account == 'acb2f3de93bb2e140a20306ebe37c9eca761d8414007eb7ad65917b31a584d23'
-         || account == 'be37ef39e88309b8b3877509d49850342647fda107f0463c17b0b4f04d86aa1a'
-         || account == '04f5ed88a931e95c38fd0477e6c52c781c6ab041d4a67b4b8663764c50813319'
-         || account == '62f15fae5a1ce17dd191d8f2d19776816548d92be01ecd0570d47e47bb951f74'
+        total += balance;
+
+        if (account == '608a961b5e9453cf703b18485cfcfb1ad9aed9930da3f80656f27a6ac12b135e' //Marketing budget
+         || account == '62f15fae5a1ce17dd191d8f2d19776816548d92be01ecd0570d47e47bb951f74' //Private sale
          || account == '3e10c920b726d9b35181bd565ffafbbba0e8ee1612872c996043fa01fcea6375'
+         || account == '04f5ed88a931e95c38fd0477e6c52c781c6ab041d4a67b4b8663764c50813319'
+         || account == '52b9d3d72ca1f13c80413a8df2de1df2248896ca46910c7ceb9c0ee5afc4c763'
+         || account == 'c27e8fc505b3b14f64b4158f39ebbb761d4f91f74ff0280127d668cbe9954e54'
+         || account == 'be37ef39e88309b8b3877509d49850342647fda107f0463c17b0b4f04d86aa1a'
+         || account == '80353aeab8323b23fc9e5f975f675957765aefd09b9be9b9eefe8c199000bc65'
+         || account == 'c669ce7c53a881d0a6e8f92cf2f41be1827947f79aa31ca39019cc9524f37623'
+         || account == 'acb2f3de93bb2e140a20306ebe37c9eca761d8414007eb7ad65917b31a584d23'
+         || account == 'da0153d9e10c805a739c40b14091186a0c92239ee414b24a6d19e89db8736a4e'
+         || account == 'fa343005718eb4c78a10e6e4515726ad0d870f268a10dbf799e9c81db66fb344'
         )
-        official += BigInt(user[1].data.free);
+        official += balance;
         else
-        totalTransferable += BigInt(transferable);
+        totalTransferable += transferable;
 
         if (account == '6d6f646c70792f74727372790000000000000000000000000000000000000000'
        )
-       treasury += BigInt(user[1].data.free);
+       treasury += balance;
 
        ++addressCount;
-       if(user[1].data.free >= 1n)
+       if(balance >= 1000000000000n)
         ++addressCountMore1;
-        if(user[1].data.free >= 999000000000000000n){
+        if(balance >= 999000000000000000n){
         ++addressCountMore1M;
-        totalM += BigInt(user[1].data.free);
+        totalM += balance;
         }
 
-        list.push(BigInt(user[1].data.free));
+        list.push(balance);
     }
 
     var total99 = BigInt((parseFloat(total.toString()) * 0.99).toFixed(0).toString());
